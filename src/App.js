fdeda05f05c6001c9b8dd4ca304d16f1c964388f
sadiq79 <sadiq.md.ali@gmail.com>
2019-04-09 12:39:04 +0530

    Map.Js added File and imported

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+import { GoogleApiWrapper} from 'google-maps-react';
+import CurrentLocation from './Map'
 
 export class MapContainer extends Component {
   render() {
     return (
-      <Map
-        google= { this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233
-        }}
-      />
+      <div>
+        <CurrentLocation/>
+      </div>
+      
     );
   }
 }
